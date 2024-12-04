75067aa7011f1e9bee49e7b81563fb9efdf6f114
K-Soo <kanabun102@naver.com>
2024-12-05 00:59:26 +0900

    refactor: Remove unused database error constants from custom HTTP exception

--- src/common/constants/custom-http-exception.ts
+++ src/common/constants/custom-http-exception.ts
@@ -11,16 +11,4 @@ export const customHttpException = {
     customCode: 1020,
     message: 'The requested resource was not found.',
   },
-  DATABASE_QUERY_FAILED: {
-    customCode: 6000,
-    message: 'An error occurred while executing a database query.',
-  },
-  DATABASE_INTEGRITY_VIOLATION: {
-    customCode: 6010,
-    message: 'Data integrity violation in the database.',
-  },
-  DATABASE_CONNECTION_FAILED: {
-    customCode: 6020,
-    message: 'Failed to connect to the database.',
-  },
-};
+} as const;
