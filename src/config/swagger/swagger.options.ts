108af1eaf9c6dc421c8352f4f0616224b7a16ce3
K-Soo <kanabun102@naver.com>
2024-12-13 01:20:55 +0900

    feat: update API title in Swagger configuration to reflect Hotel Job Connect

--- src/config/swagger/swagger.options.ts
+++ src/config/swagger/swagger.options.ts
@@ -1,7 +1,7 @@
 import { DocumentBuilder } from '@nestjs/swagger';
 
 export const swaggerOptions = new DocumentBuilder()
-  .setTitle('My App API')
+  .setTitle('Hotel Job Connect API')
   .setDescription('API documentation for My App')
   .setVersion('1.0')
   .addBearerAuth()
