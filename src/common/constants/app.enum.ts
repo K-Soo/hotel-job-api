de8b76b54e8cff5ace96c980fb5ffd02c164b045
K-Soo <kanabun102@naver.com>
2024-12-13 02:09:09 +0900

    refactor: rename ProviderRole to Provider and UserRole to Role for clarity

--- src/common/constants/app.enum.ts
+++ src/common/constants/app.enum.ts
@@ -1,9 +1,9 @@
-export enum ProviderRole {
+export enum Provider {
   LOCAL = 'LOCAL',
   KAKAO = 'KAKAO',
 }
 
-export enum UserRole {
+export enum Role {
   ADMIN = 'ADMIN',
   EMPLOYER = 'EMPLOYER',
   JOB_SEEKER = 'JOB_SEEKER',
