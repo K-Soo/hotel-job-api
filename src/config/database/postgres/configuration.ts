8f23fa577bf67e3e4f4eb082901dc7c0474924bb
K-Soo <kanabun102@naver.com>
2024-12-14 00:46:32 +0900

    feat: enhance database connection with logging and error handling; update employer entity to use new role and provider enums

--- src/config/database/postgres/configuration.ts
+++ src/config/database/postgres/configuration.ts
@@ -1,27 +1,38 @@
+import { Logger } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
 import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
-
+import { DataSource } from 'typeorm';
 export const configuration: TypeOrmModuleAsyncOptions = {
   imports: [ConfigModule],
   inject: [ConfigService],
   useFactory: async (configService: ConfigService) => {
     const isLocal = configService.get('APP_ENV') === 'local';
-    return {
+    const logger = new Logger('DatabaseConnection');
+    const dataSource = new DataSource({
       type: 'postgres',
       host: configService.get('DB_HOST'),
-      port: configService.get('DB_PORT'),
+      port: configService.get<number>('DB_PORT'),
       username: configService.get('DB_USERNAME'),
       password: configService.get('DB_PASSWORD'),
       database: configService.get('DB_NAME'),
-      retryAttempts: 1,
       entities: ['dist/**/*.entity.js'],
-      namingStrategy: new SnakeNamingStrategy(), // 스네이크케이스로 변환
+      namingStrategy: new SnakeNamingStrategy(),
       synchronize: isLocal,
-      connectTimeoutMS: 1000, // 1ms로 강제 타임아웃 설정
-
+      connectTimeoutMS: 1000,
       ...(!isLocal && { ssl: { rejectUnauthorized: false } }),
-      // autoLoadEntities: true, // 개쌉중요해 EntityMetadataNotFoundError 에러 났었음.
+    });
+
+    try {
+      await dataSource.initialize();
+      logger.log('----- Database connection established successfully! -----');
+    } catch (error) {
+      logger.error('Failed to connect to the database', error);
+      throw error;
+    }
+
+    return {
+      ...dataSource.options,
     };
   },
 };
