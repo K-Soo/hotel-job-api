10053acd7da7f8fa06ce5c732b3da256211d3a42
K-Soo <kanabun102@naver.com>
2024-12-16 22:25:18 +0900

    feat: extend CreateResumeDto with additional fields and validation, update ResumesController and ResumesService for enhanced resume management

--- src/modules/resumes/resumes.module.ts
+++ src/modules/resumes/resumes.module.ts
@@ -4,8 +4,12 @@ import { ResumesController } from './resumes.controller';
 import { Resume } from './entities/resume.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ApplicantsModule } from '../applicants/applicants.module';
+import { ExperiencesModule } from '../experiences/experiences.module';
+import { LicensesModule } from '../licenses/licenses.module';
+import { MilitaryModule } from '../military/military.module';
+
 @Module({
-  imports: [TypeOrmModule.forFeature([Resume]), ApplicantsModule],
+  imports: [TypeOrmModule.forFeature([Resume]), ApplicantsModule, ExperiencesModule, LicensesModule, MilitaryModule],
   controllers: [ResumesController],
   providers: [ResumesService],
 })
