821d4a0fae7cdedf492c9ca4257f6d343f67ac17
K-Soo <kanabun102@naver.com>
2024-12-20 00:42:59 +0900

    feat: add ProficiencyType to common types

--- src/common/types/index.ts
+++ src/common/types/index.ts
@@ -1,4 +1,5 @@
-import { Role, Provider } from '../constants/app.enum';
+import { Role, Provider, Proficiency } from '../constants/app.enum';
 
 export type RoleType = `${Role}`;
 export type ProviderType = `${Provider}`;
+export type ProficiencyType = `${Proficiency}`;
