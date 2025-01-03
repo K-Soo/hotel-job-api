28dac82afa9215ea8d7832f3f938aa5cfea7589a
K-Soo <kanabun102@naver.com>
2025-01-04 07:07:54 +0900

    feat: EmployerUser 인터페이스에서 name 속성 제거

--- src/common/interfaces/user.interface.ts
+++ src/common/interfaces/user.interface.ts
@@ -3,7 +3,6 @@ import { ProviderType, RoleType } from '../types';
 export interface EmployerUser {
   id: string;
   userId: string;
-  name: string;
   password: string;
   provider: ProviderType;
   role: RoleType;
