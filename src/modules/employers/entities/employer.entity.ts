956405bace1bdbbcb57515ac65971056cac5597e
K-Soo <kanabun102@naver.com>
2025-01-04 07:09:52 +0900

    feat: 고용주 엔티티에 회사 및 동의 관계 추가, 고유한 닉네임 생성 로직 구현

--- src/modules/employers/entities/employer.entity.ts
+++ src/modules/employers/entities/employer.entity.ts
@@ -1,17 +1,37 @@
-import { Column, CreateDateColumn, Entity, OneToOne, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+import {
+  BeforeInsert,
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 import { Consent } from '../../consents/entities/consent.entity';
-import { Role, Provider } from '../../../common/constants/app.enum';
+import { Role, Provider, AccountStatus, VerificationStatus } from '../../../common/constants/app.enum';
+import { Company } from '../company/entities/company.entity';
+import { Exclude } from 'class-transformer';
+
+// 8자리 난수 추가
+function generateRandom8Digit(): string {
+  return Math.floor(10000000 + Math.random() * 90000000).toString();
+}
+
 @Entity()
 export class Employer {
+  @OneToOne(() => Consent, (consent) => consent.employer)
+  consent: Consent;
+
+  @OneToOne(() => Company, (company) => company.employer)
+  company: Company;
+
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
   @Column()
   userId: string;
 
-  @Column()
-  name: string;
-
+  @Exclude()
   @Column()
   password: string;
 
@@ -21,21 +41,32 @@ export class Employer {
   @Column({ type: 'enum', enum: Role, default: Role.EMPLOYER })
   role: Role;
 
+  @Column({ type: 'enum', enum: AccountStatus, default: AccountStatus.ACTIVE })
+  accountStatus: AccountStatus;
+
+  @Column({ type: 'enum', enum: VerificationStatus, default: VerificationStatus.NOT_REQUESTED })
+  companyVerificationStatus: VerificationStatus;
+
+  @Column({ unique: true })
+  nickname: string;
+
+  @BeforeInsert()
+  async generateUniqueNickname() {
+    const adjectives = ['열정적인', '귀여운', '사랑스러운', '활기찬', '배고픈'];
+    const nouns = ['호랑이', '하마', '고양이', '강아지', '늑대'];
+
+    const randomAdjective = adjectives[Math.floor(Math.random() * adjectives.length)];
+    const randomNoun = nouns[Math.floor(Math.random() * nouns.length)];
+    const randomNumber = generateRandom8Digit();
+    this.nickname = `${randomAdjective}${randomNoun}${randomNumber}`;
+  }
+
   @CreateDateColumn({ type: 'timestamptz', precision: 0 })
   createdAt: Date;
 
   @UpdateDateColumn({ type: 'timestamptz', precision: 0 })
   updatedAt: Date;
 
-  // @OneToOne(() => Consent, (consent) => consent.employer)
-  // consent: Consent;
-}
-
-function generateRandomUsername(): string {
-  const adjectives = ['열정적인', '귀여운', '사랑스런', '활기찬', '배고픈'];
-  const nouns = ['호랑이', '하마', '고양이', '강아지', '늑대'];
-  const randomAdjective = adjectives[Math.floor(Math.random() * adjectives.length)];
-  const randomNoun = nouns[Math.floor(Math.random() * nouns.length)];
-
-  return `${randomAdjective} ${randomNoun}`;
+  @Column({ type: 'timestamptz', precision: 0, nullable: true })
+  passwordChangedAt: Date | null;
 }
