6678701b3088cf2dc74473f6f92d190c51da75e3
K-Soo <kanabun102@naver.com>
2025-01-04 07:07:12 +0900

    feat: UsersModule을 추가하여 OAuth 모듈의 의존성 확장

--- src/authentication/oauth/oauth.module.ts
+++ src/authentication/oauth/oauth.module.ts
@@ -7,8 +7,9 @@ import { AuthModule } from '../auth/auth.module';
 import { ApplicantsModule } from '../../modules/applicants/applicants.module';
 import { JwtModule } from '@nestjs/jwt';
 import { ConsentsModule } from '../../modules/consents/consents.module';
+import { UsersModule } from '../../modules/users/users.module';
 @Module({
-  imports: [HttpModule, AuthModule, ApplicantsModule, JwtModule, ConsentsModule],
+  imports: [HttpModule, AuthModule, ApplicantsModule, JwtModule, ConsentsModule, UsersModule],
   controllers: [OauthController],
   providers: [OauthService, KakaoCustomStrategy],
 })
