0fc10f314f1801aa26e20e9e76e53d09699e32d0
K-Soo <kanabun102@naver.com>
2025-01-04 07:07:33 +0900

    feat: 사용자 관련 예외 처리 메시지 및 코드 추가

--- src/common/constants/custom-http-exception.ts
+++ src/common/constants/custom-http-exception.ts
@@ -11,10 +11,7 @@ export const customHttpException = {
     customCode: 1002,
     message: 'Access token is invalid',
   },
-  USER_ROLE_INVALID: {
-    customCode: 1010,
-    message: 'Permission denied',
-  },
+
   REFRESH_TOKEN_EXPIRED: {
     customCode: 1020,
     message: 'Refresh token expired',
@@ -27,6 +24,17 @@ export const customHttpException = {
     customCode: 1022,
     message: 'Refresh token is invalid credentials',
   },
+
+  NOT_FOUND_USER: {
+    customCode: 1030,
+    message: 'Not found User.',
+  },
+
+  USER_ROLE_INVALID: {
+    customCode: 1040,
+    message: 'Permission denied',
+  },
+
   OAUTH_SIGN_IN_TOKEN: {
     customCode: 2000,
     message: 'Authentication failed',
@@ -35,6 +43,7 @@ export const customHttpException = {
     customCode: 2001,
     message: 'Authentication failed.',
   },
+
   OAUTH_SIGN_IN_NOT_FOUND_USER: {
     customCode: 2002,
     message: 'User not found',
@@ -47,8 +56,26 @@ export const customHttpException = {
     customCode: 3000,
     message: 'Creation limit exceeded for the resource',
   },
+  //데이터베이스
   DATABASE_OPERATION_FAILED: {
     customCode: 6000,
     message: 'An error occurred while processing your request',
+  },
+  //파일
+  IMAGE_UPLOAD_FAILED: {
+    customCode: 7000,
+    message: 'Failed to process the file. Please try again.',
+  },
+  IMAGE_DELETE_FAILED: {
+    customCode: 7001,
+    message: 'Failed to process the file. Please try again.',
+  },
+  IMAGE_FORMAT_NOT_SUPPORTED: {
+    customCode: 7002,
+    message: 'Failed to process the file. Please try again.',
+  },
+  IMAGE_SIZE_EXCEEDED: {
+    customCode: 7003,
+    message: 'Failed to process the file. Please try again.',
   },
 } as const;
