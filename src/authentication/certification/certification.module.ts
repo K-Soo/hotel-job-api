1538ec7af81c551aae6eb85df4a58337bba4c881
K-Soo <kanabun102@naver.com>
2025-01-08 00:58:45 +0900

    feat: 인증 모듈에 TypeORM 및 관련 엔티티 추가

--- src/authentication/certification/certification.module.ts
+++ src/authentication/certification/certification.module.ts
@@ -3,9 +3,22 @@ import { CertificationService } from './certification.service';
 import { CertificationController } from './certification.controller';
 import { SecretsManagerModule } from '../../providers/secrets-manager/secrets-manager.module';
 import { HttpModule } from '@nestjs/axios';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Certification } from './entities/certification.entity';
+import { EmployersModule } from '../../modules/employers/employers.module';
+import { Employer } from '../../modules/employers/entities/employer.entity';
+import { ApplicantsModule } from '../../modules/applicants/applicants.module';
+import { Applicant } from '../../modules/applicants/entities/applicant.entity';
+import { AuthModule } from '../auth/auth.module';
 @Module({
-  imports: [SecretsManagerModule, HttpModule],
+  imports: [
+    TypeOrmModule.forFeature([Certification, Employer, Applicant]),
+    SecretsManagerModule,
+    AuthModule,
+    HttpModule,
+    EmployersModule,
+    ApplicantsModule,
+  ],
   controllers: [CertificationController],
   providers: [CertificationService],
 })
