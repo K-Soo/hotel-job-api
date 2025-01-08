3fc7c665d53b112981cb5c01bb1369d0f5af4f56
K-Soo <kanabun102@naver.com>
2025-01-08 16:52:30 +0900

    feat: RecruitmentModule을 EmployersModule에 추가

--- src/modules/employers/employers.module.ts
+++ src/modules/employers/employers.module.ts
@@ -5,9 +5,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Employer } from './entities/employer.entity';
 import { AuthModule } from '../../authentication/auth/auth.module';
 import { CompanyModule } from './company/company.module';
+import { RecruitmentModule } from './recruitment/recruitment.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Employer]), forwardRef(() => AuthModule), CompanyModule],
+  imports: [TypeOrmModule.forFeature([Employer]), forwardRef(() => AuthModule), CompanyModule, RecruitmentModule],
   controllers: [EmployersController],
   providers: [EmployersService],
   exports: [EmployersService],
