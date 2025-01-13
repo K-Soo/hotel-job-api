4a4fabc2acec1a70ea91934bdbe4b0be488f5a12
K-Soo <kanabun102@naver.com>
2025-01-14 05:35:13 +0900

    feat: LocationInfoDto 클래스 추가 및 필드 정의

---
+++ src/modules/employers/recruitment/dto/location-info.dto.ts
@@ -0,0 +1,23 @@
+import { IsNotEmpty, IsString, IsNumber } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { Expose } from 'class-transformer';
+
+export class LocationInfoDto {
+  @ApiProperty({ description: '객실 수', example: '홍길동' })
+  @Expose()
+  @IsNumber()
+  @IsNotEmpty()
+  roomCount: number;
+
+  @ApiProperty({ description: '주소', example: '서울 특별시' })
+  @Expose()
+  @IsString()
+  @IsNotEmpty()
+  address: string;
+
+  @ApiProperty({ description: '주소 상세', example: '서울특별시' })
+  @Expose()
+  @IsString()
+  @IsNotEmpty()
+  addressDetail: string;
+}
