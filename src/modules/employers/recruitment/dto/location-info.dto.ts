b50b113ce1fcc1450059561b460483669bd83280
K-Soo <kanabun102@naver.com>
2025-01-15 04:46:33 +0900

    feat: locationInfo.dto에 hotelName 속성 추가

--- src/modules/employers/recruitment/dto/location-info.dto.ts
+++ src/modules/employers/recruitment/dto/location-info.dto.ts
@@ -3,6 +3,12 @@ import { ApiProperty } from '@nestjs/swagger';
 import { Expose } from 'class-transformer';
 
 export class LocationInfoDto {
+  @ApiProperty({ description: '호텔명', example: '서울호텔' })
+  @Expose()
+  @IsString()
+  @IsNotEmpty()
+  hotelName: string;
+
   @ApiProperty({ description: '객실 수', example: '홍길동' })
   @Expose()
   @IsNumber()
