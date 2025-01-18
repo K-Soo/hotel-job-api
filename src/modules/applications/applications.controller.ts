69a9d50ee0a184140b3fc75dfd9f2d764606bc2d
K-Soo <kanabun102@naver.com>
2025-01-19 03:12:19 +0900

    feat: 채용공고 지원 관련 API 엔드포인트 추가 및 Swagger 문서화

--- src/modules/applications/applications.controller.ts
+++ src/modules/applications/applications.controller.ts
@@ -4,10 +4,11 @@ import { ApplyResumeDto } from './dto/apply-resume.dto';
 import { RolesGuard } from '../../common/guards/roles.guard';
 import { PassportJwtGuard } from '../../authentication/auth/guards/passport-jwt.guard';
 import { Roles } from '../../common/decorators/metadata/roles.decorator';
-import { ApiBearerAuth } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation, ApiParam } from '@nestjs/swagger';
 import { Request } from 'express';
 import { ApplicantsService } from '../applicants/applicants.service';
 import { ResponseStatus } from '../../common/constants/responseStatus';
+import { EmployersService } from '../employers/employers.service';
 
 @ApiBearerAuth()
 @Controller('applications')
@@ -17,8 +18,10 @@ export class ApplicationsController {
   constructor(
     private readonly applicationsService: ApplicationsService,
     private readonly applicantsService: ApplicantsService,
+    private readonly employersService: EmployersService,
   ) {}
 
+  @ApiOperation({ summary: '채용공고 지원하기' })
   @Post('/apply')
   async createApplyResume(@Req() req: Request, @Body() applyResumeDto: ApplyResumeDto) {
     const userUuid = req.user['uuid'];
@@ -26,6 +29,7 @@ export class ApplicationsController {
     return this.applicationsService.applyResume(applyResumeDto, applicant);
   }
 
+  @ApiOperation({ summary: '채용공고 이력서 지원여부 체크' })
   @Get(':id/apply/check')
   async checkIfAlreadyApplied(@Req() req: Request, @Param('id') recruitId: string) {
     const userUuid = req.user['uuid'];
@@ -39,10 +43,23 @@ export class ApplicationsController {
     return { status: ResponseStatus.AVAILABLE };
   }
 
-  @Get()
-  async getApplication(@Req() req: Request) {
+  @ApiOperation({ summary: '채용공고 별 지원자 리스트', description: '채용공고 별 지원자 리스트를 조회합니다.' })
+  @ApiParam({
+    name: 'id',
+    type: String,
+    description: 'Recruitment UUID',
+    example: 'fbd01641-24f6-4e61-a727-c59c6ecc41ef',
+    required: true,
+  })
+  @Get('/recruitment/:id')
+  async getApplicationsForRecruitment(@Req() req: Request, @Param('id') recruitmentId: string) {
     const userUuid = req.user['uuid'];
+    const employer = await this.employersService.findOneUuid(userUuid);
 
-    return { status: ResponseStatus.AVAILABLE };
+    return this.applicationsService.getApplicationsForRecruitment(recruitmentId, employer);
   }
+
+  @ApiOperation({ summary: '특정 채용공고 상태 별 수량값' })
+  @Get('/recruitment/status')
+  async getApplicationRecruitmentStatus() {}
 }
