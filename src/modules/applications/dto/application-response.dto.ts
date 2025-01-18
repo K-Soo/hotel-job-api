b1dd30cf24f6b8eff52421f7e3a7e6eea29f23a7
K-Soo <kanabun102@naver.com>
2025-01-19 00:24:32 +0900

    feat: ApplicationResponseDto 추가 및 응답 데이터 구조 정의

---
+++ src/modules/applications/dto/application-response.dto.ts
@@ -0,0 +1,13 @@
+import { Expose } from 'class-transformer';
+import { ApplicationStatus } from '../../../common/constants/application';
+
+export class ApplicationResponseDto {
+  @Expose()
+  id: string;
+
+  @Expose()
+  applicationStatus: ApplicationStatus;
+
+  @Expose()
+  createdAt: Date;
+}
