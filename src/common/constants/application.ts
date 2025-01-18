897606b1335eebe8127f9279cc73b30cec250b4f
K-Soo <kanabun102@naver.com>
2025-01-19 00:24:06 +0900

    feat: 지원서 상태 및 심사 단계 상태를 나타내는 상수 열거형 추가

---
+++ src/common/constants/application.ts
@@ -0,0 +1,11 @@
+export enum ApplicationStatus {
+  APPLIED = 'APPLIED', // 지원 완료
+  CANCELED = 'CANCELED', // 지원 취소
+}
+
+export enum ReviewStageStatus {
+  DOCUMENT = 'DOCUMENT', // 서류전형
+  INTERVIEW = 'INTERVIEW', // 면접전형
+  FINAL_ACCEPTED = 'FINAL_ACCEPTED', // 최종 합격
+  REJECTED = 'REJECTED', // 불합격
+}
