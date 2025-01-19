4c387a69e29aa8494f8c9956ca0266bc1b912066
K-Soo <kanabun102@naver.com>
2025-01-19 20:47:28 +0900

    feat: ApplicantsService에 계정 비활성화 기능 추가 및 계정 상태 처리 로직 개선

--- src/modules/applicants/applicants.service.ts
+++ src/modules/applicants/applicants.service.ts
@@ -1,20 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Applicant } from './entities/applicant.entity';
 import { safeQuery } from '../../common/helpers/database.helper';
-import { Provider, Role } from '../../common/constants/app.enum';
+import { AccountStatus, Provider, Role } from '../../common/constants/app.enum';
+import { ResponseStatus } from '../../common/constants/responseStatus';
+import { AccountHistoryService } from '../../authentication/account-history/account-history.service';
+import { customHttpException } from '../../common/constants/custom-http-exception';
+import { handleAccountStatus } from '../../common/helpers/account.helper';
 @Injectable()
 export class ApplicantsService {
-  constructor(@InjectRepository(Applicant) private repo: Repository<Applicant>) {}
+  constructor(
+    @InjectRepository(Applicant) private repo: Repository<Applicant>,
+    private readonly accountHistoryService: AccountHistoryService,
+  ) {}
 
   async create(userId: string, email: string, provider: Provider) {
     const user = await safeQuery(async () => this.repo.create({ userId, provider, email, role: Role.JOB_SEEKER }));
     return this.repo.save(user);
-  }
-
-  findAll() {
-    return `This action returns all applicants`;
   }
 
   async findOne(id: string) {
@@ -24,11 +27,29 @@ export class ApplicantsService {
     return applicant;
   }
 
-  findByUserId(userId: string) {
-    return safeQuery(() => this.repo.findOne({ where: { userId: userId } }));
+  // 소셜 계정 고유 id로 유저 찾기
+  async findByUserId(userId: string) {
+    const user = await safeQuery(() => this.repo.findOne({ where: { userId: userId } }));
+
+    handleAccountStatus(user.accountStatus);
+
+    return user;
   }
 
   findByUuid(uuid: string) {
     return safeQuery(() => this.repo.findOne({ where: { id: uuid } }));
+  }
+
+  async deactivatedForApplicant(uuid: string) {
+    const applicant = await safeQuery(() => this.repo.findOne({ where: { id: uuid } }));
+
+    await this.accountHistoryService.changeAccountStatus(
+      applicant,
+      AccountStatus.DEACTIVATED,
+      applicant.userId,
+      'ADMIN',
+    );
+
+    return { status: ResponseStatus.SUCCESS };
   }
 }
