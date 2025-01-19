97bb126fb8d267d4fc5f14df6795891ed67607da
K-Soo <kanabun102@naver.com>
2025-01-19 20:47:23 +0900

    feat: 계정 삭제 요청 기능 추가

--- src/modules/applicants/applicants.controller.ts
+++ src/modules/applicants/applicants.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, Delete, Req, UseGuards } from '@nestjs/common';
 import { ApplicantsService } from './applicants.service';
 import { ApiOperation, ApiBearerAuth } from '@nestjs/swagger';
 import { PassportJwtGuard } from '../../authentication/auth/guards/passport-jwt.guard';
@@ -9,11 +9,6 @@ import { RolesGuard } from '../../common/guards/roles.guard';
 export class ApplicantsController {
   constructor(private readonly applicantsService: ApplicantsService) {}
 
-  @Get()
-  findAll() {
-    return this.applicantsService.findAll();
-  }
-
   @ApiOperation({ summary: '일반 유저 정보' })
   @ApiBearerAuth()
   @UseGuards(PassportJwtGuard, RolesGuard)
@@ -21,5 +16,14 @@ export class ApplicantsController {
   @Roles('JOB_SEEKER')
   find(@Req() req: Request) {
     return this.applicantsService.findOne(req.user['sub']);
+  }
+
+  @ApiOperation({ summary: '계정 삭제요청' })
+  @ApiBearerAuth()
+  @UseGuards(PassportJwtGuard, RolesGuard)
+  @Delete('deactivate')
+  @Roles('JOB_SEEKER')
+  deactivatedUser(@Req() req: Request) {
+    return this.applicantsService.deactivatedForApplicant(req.user['sub']);
   }
 }
