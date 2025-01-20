6e8cffebc7d3b70712752f5d50a4e5845f5dec76
K-Soo <kanabun102@naver.com>
2025-01-20 18:22:47 +0900

    feat: 채용공고 상태별 지원자 통계 및 전형 상태 변경 기능 추가

--- src/modules/applications/applications.controller.ts
+++ src/modules/applications/applications.controller.ts
@@ -1,15 +1,16 @@
-import { Body, Controller, Get, Param, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post, Query, Req, UseGuards } from '@nestjs/common';
 import { ApplicationsService } from './applications.service';
 import { ApplyResumeDto } from './dto/apply-resume.dto';
 import { RolesGuard } from '../../common/guards/roles.guard';
 import { PassportJwtGuard } from '../../authentication/auth/guards/passport-jwt.guard';
 import { Roles } from '../../common/decorators/metadata/roles.decorator';
-import { ApiBearerAuth, ApiOperation, ApiParam } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation, ApiParam, ApiQuery } from '@nestjs/swagger';
 import { Request } from 'express';
 import { ApplicantsService } from '../applicants/applicants.service';
 import { ResponseStatus } from '../../common/constants/responseStatus';
 import { EmployersService } from '../employers/employers.service';
-
+import { ReviewStageStatus } from '../../common/constants/application';
+import { UpdateReviewStageDto } from './dto/update-review-stage.dto';
 @ApiBearerAuth()
 @Controller('applications')
 @UseGuards(PassportJwtGuard, RolesGuard)
@@ -42,6 +43,22 @@ export class ApplicationsController {
     return { status: ResponseStatus.AVAILABLE };
   }
 
+  @ApiOperation({ summary: '채용공고 상태별 지원자 통계' })
+  @ApiParam({
+    name: 'id',
+    type: String,
+    description: 'Recruitment UUID',
+    example: 'a14d22fb-7ca7-47a1-b4fe-88bd0edd57e0',
+    required: true,
+  })
+  @Roles('EMPLOYER')
+  @Get('/recruitment/:id/status')
+  async getRecruitmentApplicationStatusCount(@Req() req: Request, @Param('id') recruitmentId: string) {
+    const employer = await this.employersService.findOneUuid(req.user['sub']);
+
+    return this.applicationsService.calculateEmployerReviewStageStatusCount(recruitmentId, employer);
+  }
+
   @ApiOperation({ summary: '채용공고 별 지원자 리스트', description: '채용공고 별 지원자 리스트를 조회합니다.' })
   @ApiParam({
     name: 'id',
@@ -50,23 +67,36 @@ export class ApplicationsController {
     example: 'fbd01641-24f6-4e61-a727-c59c6ecc41ef',
     required: true,
   })
+  @ApiQuery({
+    name: 'step',
+    type: String,
+    description: 'Review Stage Status (DOCUMENT, INTERVIEW, FINAL_ACCEPT, REJECT)',
+    required: false,
+  })
   @Roles('EMPLOYER')
   @Get('/recruitment/:id')
-  async getApplicationsForRecruitment(@Req() req: Request, @Param('id') recruitmentId: string) {
+  async getApplicationsForRecruitment(
+    @Req() req: Request,
+    @Param('id') recruitmentId: string,
+    @Query('step') step?: ReviewStageStatus,
+  ) {
     const employer = await this.employersService.findOneUuid(req.user['sub']);
 
-    return this.applicationsService.getApplicationsForRecruitment(recruitmentId, employer);
+    return this.applicationsService.getApplicationsForRecruitment(recruitmentId, employer, step);
   }
-
-  @ApiOperation({ summary: '특정 채용공고 상태 별 수량값' })
-  @Roles('EMPLOYER')
-  @Get('/recruitment/status')
-  async getApplicationRecruitmentStatus() {}
 
   @ApiOperation({ summary: '채용공고 지원내역' })
   @Roles('JOB_SEEKER')
   @Get('/history')
   async getUserApplicationsHistory(@Req() req: Request) {
     return this.applicationsService.calculateApplicationHistory(req.user['sub']);
+  }
+
+  @ApiOperation({ summary: '전형 상태 변경', description: '지원자의 전형 상태를 변경합니다.' })
+  @Roles('EMPLOYER')
+  @Patch('/recruitment/status/review-stage')
+  async updateEmployerReviewStageStatus(@Req() req: Request, @Body() updateReviewStageDto: UpdateReviewStageDto) {
+    const employer = await this.employersService.findOneUuid(req.user['sub']);
+    return this.applicationsService.updateEmployerReviewStageStatus(updateReviewStageDto, employer);
   }
 }
