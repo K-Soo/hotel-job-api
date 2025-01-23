e0513be056904597738a6120daf3c964a432184d
K-Soo <kanabun102@naver.com>
2025-01-24 03:04:53 +0900

    fix: ApplicationResponseDto에 reviewStageStatus 및 recruitment 필드 추가

--- src/modules/applications/dto/application-response.dto.ts
+++ src/modules/applications/dto/application-response.dto.ts
@@ -1,6 +1,7 @@
-import { Expose } from 'class-transformer';
-import { ApplicationStatus } from '../../../common/constants/application';
-
+import { Expose, Type } from 'class-transformer';
+import { ApplicationStatus, ReviewStageStatus } from '../../../common/constants/application';
+import { ApplicationRecruitmentResponseDto } from './application-recruitment-response.dto';
+import { ValidateNested } from 'class-validator';
 export class ApplicationResponseDto {
   @Expose()
   id: string;
@@ -9,8 +10,19 @@ export class ApplicationResponseDto {
   applicationStatus: ApplicationStatus;
 
   @Expose()
+  reviewStageStatus: ReviewStageStatus;
+
+  @Expose()
   createdAt: Date;
 
   @Expose()
   applyAt: Date;
+
+  @Expose()
+  cancelAt: Date;
+
+  @Expose()
+  @Type(() => ApplicationRecruitmentResponseDto)
+  @ValidateNested({ each: true })
+  recruitment: ApplicationRecruitmentResponseDto;
 }
