899ec12d02d4b9b9c8a627b950041eb793ba02b4
K-Soo <kanabun102@naver.com>
2025-01-25 19:01:16 +0900

    feat: 채용 상품 목록 조회 기능 추가

--- src/modules/products/products.service.ts
+++ src/modules/products/products.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { RecruitmentProduct } from './entities/recruitment.entity';
+import { RecruitmentProductQueryDto } from './dto/recruitment-product-query.dto';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+  constructor(
+    @InjectRepository(RecruitmentProduct)
+    private readonly recruitmentProductRepository: Repository<RecruitmentProduct>,
+  ) {}
+
+  // 채용 상품 목록 조회
+  async findRecruitmentProducts(query: RecruitmentProductQueryDto) {
+    const products = await this.recruitmentProductRepository.find({
+      where: { type: query.type },
+      relations: ['durations', 'options'],
+    });
+
+    return products;
+  }
+}
