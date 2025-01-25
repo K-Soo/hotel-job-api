25dc4dc7883c895fba4befa81ac985d9731c6181
K-Soo <kanabun102@naver.com>
2025-01-25 19:01:29 +0900

    feat: 채용 상품 엔티티 추가

---
+++ src/modules/products/entities/recruitment.entity.ts
@@ -0,0 +1,40 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn, OneToMany } from 'typeorm';
+import { RecruitmentProductType, RecruitmentProductName } from '../../../common/constants/product';
+import { RecruitmentProductDuration } from './recruitment-duration.entity';
+import { RecruitmentProductOption } from './recruitment-option.entity';
+
+@Entity('recruitment_product')
+export class RecruitmentProduct {
+  @OneToMany(() => RecruitmentProductDuration, (duration) => duration.recruitmentProduct, { cascade: true })
+  durations: RecruitmentProductDuration[];
+
+  @OneToMany(() => RecruitmentProductOption, (option) => option.recruitmentProduct, { cascade: true })
+  options: RecruitmentProductOption[];
+
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'enum', enum: RecruitmentProductName })
+  name: RecruitmentProductName;
+
+  @Column({ type: 'enum', enum: RecruitmentProductType })
+  type: RecruitmentProductType;
+
+  @Column({
+    type: 'numeric',
+    precision: 4,
+    scale: 3,
+    default: 0,
+    transformer: {
+      to: (value: number) => value,
+      from: (value: string) => parseFloat(value),
+    },
+  })
+  discountRate: number;
+
+  @CreateDateColumn({ type: 'timestamptz', precision: 0 })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamptz', precision: 0 })
+  updatedAt: Date;
+}
