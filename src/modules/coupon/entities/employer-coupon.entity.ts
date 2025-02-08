0af96a71d94f1352786e59b4e20443223f3e6d53
K-Soo <kanabun102@naver.com>
2025-02-08 11:24:51 +0900

    feat: 고용주 쿠폰 엔티티 추가

---
+++ src/modules/coupon/entities/employer-coupon.entity.ts
@@ -0,0 +1,29 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn, CreateDateColumn } from 'typeorm';
+import { Coupon } from './coupon.entity';
+import { Employer } from '../../employers/entities/employer.entity';
+
+@Entity()
+export class EmployerCoupon {
+  @ManyToOne(() => Employer, (employer) => employer.coupon, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'employer_id', referencedColumnName: 'id' })
+  employer: Employer;
+
+  @ManyToOne(() => Coupon, (coupon) => coupon.employerCoupon, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'coupon_id' })
+  coupon: Coupon;
+
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'boolean', default: false })
+  isUsed: boolean;
+
+  @Column({ type: 'timestamptz', precision: 0, nullable: true })
+  usedAt: Date | null;
+
+  @Column({ type: 'timestamptz', nullable: true, precision: 0, default: null })
+  expiresAt: Date | null; // 특정 유저에게 발급된 쿠폰의 만료일 (개별 설정 가능)
+
+  @CreateDateColumn({ type: 'timestamptz', precision: 0 })
+  createdAt: Date;
+}
