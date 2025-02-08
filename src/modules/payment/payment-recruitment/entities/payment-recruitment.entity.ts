a4c9799509965662abd4be99194dc306931943a5
K-Soo <kanabun102@naver.com>
2025-02-08 11:25:28 +0900

    feat: 결제 모집 엔티티 및 옵션 엔티티 추가

--- src/modules/payment/payment-recruitment/entities/payment-recruitment.entity.ts
+++ src/modules/payment/payment-recruitment/entities/payment-recruitment.entity.ts
@@ -1,26 +1,89 @@
-import {
-  Column,
-  CreateDateColumn,
-  Entity,
-  OneToOne,
-  ManyToOne,
-  PrimaryGeneratedColumn,
-  UpdateDateColumn,
-  JoinColumn,
-} from 'typeorm';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn, JoinColumn, OneToMany, OneToOne } from 'typeorm';
 import { Recruitment } from '../../../employers/recruitment/entities/recruitment.entity';
+import { RecruitmentDuration } from '../../../../common/constants/product';
 import { Payment } from '../../entities/payment.entity';
+import {
+  RecruitmentProductType,
+  RecruitmentProductName,
+  RecruitmentOptionTag,
+  RecruitmentProductOptionName,
+} from '../../../../common/constants/product';
 
-@Entity()
+@Entity('payment_recruitment')
 export class PaymentRecruitment {
+  @ManyToOne(() => Recruitment, (recruitment) => recruitment.paymentRecruitment, {
+    nullable: true,
+    onDelete: 'SET NULL',
+  })
+  @JoinColumn({ name: 'recruit_id' })
+  recruitment: Recruitment | null;
+
   @ManyToOne(() => Payment, { onDelete: 'CASCADE' })
   @JoinColumn({ name: 'payment_id' })
   payment: Payment;
 
-  @ManyToOne(() => Recruitment, (recruit) => recruit.payments)
-  @JoinColumn({ name: 'recruit_id' })
-  recruitment: Recruitment;
+  @OneToMany(
+    () => PaymentRecruitmentOptions,
+    (paymentRecruitmentOptions) => paymentRecruitmentOptions.paymentRecruitment,
+    { cascade: true },
+  )
+  options: PaymentRecruitmentOptions[];
 
   @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column({ type: 'enum', enum: RecruitmentProductName })
+  name: RecruitmentProductName;
+
+  @Column({ type: 'enum', enum: RecruitmentProductType })
+  type: RecruitmentProductType;
+
+  @Column({ type: 'enum', enum: RecruitmentDuration })
+  duration: RecruitmentDuration;
+
+  @Column({ type: 'int', default: 0 })
+  bonusDays: number;
+
+  @Column({ type: 'int', default: 0 })
+  price: number;
+
+  @Column({
+    type: 'numeric',
+    precision: 4,
+    scale: 3,
+    default: 0,
+    transformer: {
+      to: (value: number) => value,
+      from: (value: string) => parseFloat(value),
+    },
+  })
+  discountRate: number;
+}
+
+@Entity()
+export class PaymentRecruitmentOptions {
+  @ManyToOne(() => PaymentRecruitment, (paymentRecruitment) => paymentRecruitment.options, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'payment_recruitment_id', referencedColumnName: 'id' })
+  paymentRecruitment: PaymentRecruitment;
+
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'enum', enum: RecruitmentProductOptionName })
+  name: RecruitmentProductOptionName;
+
+  @Column({ type: 'enum', enum: RecruitmentDuration })
+  duration: RecruitmentDuration;
+
+  @Column({ type: 'int', default: 0 })
+  maxListUpPerDay: number; // 24시간 내 리스트업 최대 횟수 (기본값: 0)
+
+  @Column({ type: 'int', default: 0 })
+  listUpIntervalHours: number; // 리스트업 간 최소 간격 (단위: 시간, 기본값: 0)
+
+  @Column({ type: 'enum', enum: RecruitmentOptionTag, array: true, default: [] })
+  tags: RecruitmentOptionTag[];
+
+  @Column({ type: 'int', default: 0 })
+  price: number;
 }
