317b8a64cca0c020bea7aa584381b266eb142046
K-Soo <kanabun102@naver.com>
2025-02-16 03:53:20 +0900

    feat: 결제 내역 리스트 조회 기능 추가 및 권한 설정

--- src/modules/payment/payment.controller.ts
+++ src/modules/payment/payment.controller.ts
@@ -1,7 +1,23 @@
-import { Controller } from '@nestjs/common';
 import { PaymentService } from './payment.service';
+import { Controller, UseGuards, Req, Get } from '@nestjs/common';
+import { ApiOperation, ApiBearerAuth } from '@nestjs/swagger';
+import { Request } from 'express';
+import { Roles } from '../../common/decorators/metadata/roles.decorator';
+import { RolesGuard } from '../../common/guards/roles.guard';
+import { PassportJwtGuard } from '../../authentication/auth/guards/passport-jwt.guard';
 
+@ApiBearerAuth()
+@UseGuards(PassportJwtGuard, RolesGuard)
+@Roles('EMPLOYER')
 @Controller('payment')
 export class PaymentController {
   constructor(private readonly paymentService: PaymentService) {}
+
+  @ApiOperation({ summary: '상품 결제내역 리스트' })
+  @Get()
+  async paymentList(@Req() req: Request) {
+    const userId = req.user['sub'];
+
+    return this.paymentService.paymentList(userId);
+  }
 }
