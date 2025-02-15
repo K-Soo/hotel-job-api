b92429f28f7188b465583c05f0ee5c761a794759
K-Soo <kanabun102@naver.com>
2025-02-16 03:52:47 +0900

    feat: 고용주 컨트롤러에 직렬화 인터셉터 추가 및 응답 설명 수정

--- src/modules/employers/employers.controller.ts
+++ src/modules/employers/employers.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Delete, UseGuards, Req } from '@nestjs/common';
+import { Controller, Get, Param, Delete, UseGuards, Req, UseInterceptors } from '@nestjs/common';
 import { EmployersService } from './employers.service';
 import { EmployerResponseDto } from './dto/employer.response.dto';
 import { Request } from 'express';
@@ -6,6 +6,7 @@ import { ApiOperation, ApiBearerAuth, ApiResponse } from '@nestjs/swagger';
 import { RolesGuard } from '../../common/guards/roles.guard';
 import { PassportJwtGuard } from '../../authentication/auth/guards/passport-jwt.guard';
 import { Roles } from '../../common/decorators/metadata/roles.decorator';
+import { SerializeInterceptor } from '../../common/interceptors/serialize.interceptor';
 
 @ApiBearerAuth()
 @UseGuards(PassportJwtGuard, RolesGuard)
@@ -17,10 +18,10 @@ export class EmployersController {
   @ApiOperation({ summary: '계정정보' })
   @ApiResponse({
     status: 200,
-    description: '계정정보 응답값',
+    description: '계정정보',
     type: EmployerResponseDto,
   })
-  // @UseInterceptors(new SerializeInterceptor(EmployerResponseDto, { groups: ['account'] }))
+  @UseInterceptors(new SerializeInterceptor(EmployerResponseDto, { groups: ['account'] }))
   @Get()
   accountInfo(@Req() req: Request) {
     return this.employersService.accountInfo(req.user['sub']);
