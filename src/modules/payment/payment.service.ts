4b86d77872192240da90c8062e11d3969d7a9e2e
K-Soo <kanabun102@naver.com>
2025-02-16 03:53:22 +0900

    feat: 결제 내역 조회 기능 추가 및 포맷팅 구현

--- src/modules/payment/payment.service.ts
+++ src/modules/payment/payment.service.ts
@@ -1,4 +1,50 @@
 import { Injectable } from '@nestjs/common';
-
+import { InjectRepository } from '@nestjs/typeorm';
+import { Payment } from './entities/payment.entity';
+import { Repository } from 'typeorm';
+import { PaymentStatus, PaymentType } from '../../common/constants/payment';
 @Injectable()
-export class PaymentService {}
+export class PaymentService {
+  constructor(@InjectRepository(Payment) private paymentRepo: Repository<Payment>) {}
+
+  async paymentList(userId: string) {
+    const payments = await this.paymentRepo
+      .createQueryBuilder('payment')
+      .leftJoin('payment.transactions', 'transactions')
+      .addSelect([
+        'transactions.id',
+        'transactions.totalAmount',
+        'transactions.cardType',
+        'transactions.method',
+        'transactions.orderName',
+        'transactions.installmentPlanMonths',
+        'transactions.number',
+        'transactions.approvedAt',
+        'transactions.orderId',
+      ])
+      .leftJoin('payment.recruitmentPayments', 'recruitmentPayments')
+      .addSelect(['recruitmentPayments.id'])
+      .leftJoin('recruitmentPayments.recruitment', 'recruitment')
+      .addSelect(['recruitment.id', 'recruitment.recruitmentTitle'])
+      .where('payment.userId = :userId', { userId })
+      .andWhere('payment.paymentType = :paymentType', { paymentType: PaymentType.RECRUITMENT })
+      .andWhere('payment.paymentStatus = :paymentStatus', { paymentStatus: PaymentStatus.PAYMENT_COMPLETED })
+      .orderBy('payment.createdAt', 'DESC')
+      .getMany();
+
+    const formattedPayments = payments.map((payment) => {
+      const { transactions, recruitmentPayments, ...rest } = payment;
+
+      return {
+        ...rest,
+        transactions,
+        recruitment: {
+          id: recruitmentPayments[0].recruitment.id,
+          recruitmentTitle: recruitmentPayments[0].recruitment.recruitmentTitle,
+        },
+      };
+    });
+
+    return formattedPayments;
+  }
+}
