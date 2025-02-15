0110f3c2961a7697bcc4972ec2f24bbc280070d4
K-Soo <kanabun102@naver.com>
2025-02-16 03:52:34 +0900

    feat: 결정 상태 DTO에서 최종 결정 상태 필드 제거

--- src/modules/applications/dto/update-decision-status.dto.ts
+++ src/modules/applications/dto/update-decision-status.dto.ts
@@ -1,13 +1,8 @@
-import { IsEnum, IsNumber } from 'class-validator';
-import { FinalDecisionStatus } from '../../../common/constants/application';
+import { IsNumber } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateDecisionStatusDto {
   @ApiProperty({ description: 'applicationId', example: 1 })
   @IsNumber()
   applicationId: number;
-
-  @ApiProperty({ description: 'FinalDecisionStatus', example: FinalDecisionStatus.ACCEPT, enum: FinalDecisionStatus })
-  @IsEnum(FinalDecisionStatus, { message: 'FinalDecisionStatus must be a valid value' })
-  finalDecisionStatus: FinalDecisionStatus;
 }
