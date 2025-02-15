f30045082dde7b7fddfd81c0e1d2ba872058d74e
K-Soo <kanabun102@naver.com>
2025-02-16 03:53:41 +0900

    feat: 결제 엔티티를 리크루트 모듈에 추가

--- src/modules/recruit/recruit.module.ts
+++ src/modules/recruit/recruit.module.ts
@@ -3,9 +3,9 @@ import { RecruitService } from './recruit.service';
 import { RecruitController } from './recruit.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Recruitment } from '../employers/recruitment/entities/recruitment.entity';
-
+import { Payment } from '../payment/entities/payment.entity';
 @Module({
-  imports: [TypeOrmModule.forFeature([Recruitment])],
+  imports: [TypeOrmModule.forFeature([Recruitment, Payment])],
   controllers: [RecruitController],
   providers: [RecruitService],
 })
