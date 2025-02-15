4fdda550a3b846ac83b767999fd134c9769066a3
K-Soo <kanabun102@naver.com>
2025-02-16 03:52:15 +0900

    feat: 쿠폰 관리 기능 추가 및 개선

--- src/modules/coupon/coupon.controller.ts
+++ src/modules/coupon/coupon.controller.ts
@@ -1,34 +1,34 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { CouponService } from './coupon.service';
-import { CreateCouponDto } from './dto/create-coupon.dto';
-import { UpdateCouponDto } from './dto/update-coupon.dto';
+import { ApiBearerAuth, ApiOperation, ApiQuery } from '@nestjs/swagger';
+import { RolesGuard } from '../../common/guards/roles.guard';
+import { PassportJwtGuard } from '../../authentication/auth/guards/passport-jwt.guard';
+import { Request } from 'express';
+import { CreateEmployerCouponDto } from './dto/create-employer-coupon.dto';
+import { Controller, Get, Post, Body, UseGuards, Req, Query, BadRequestException } from '@nestjs/common';
 
+@ApiBearerAuth()
+@Controller('coupon')
+@UseGuards(PassportJwtGuard, RolesGuard)
 @Controller('coupon')
 export class CouponController {
   constructor(private readonly couponService: CouponService) {}
 
-  @Post()
-  create(@Body() createCouponDto: CreateCouponDto) {
-    return this.couponService.create(createCouponDto);
+  @ApiOperation({ summary: '쿠폰 리스트' })
+  @Get('/employer')
+  @ApiQuery({ name: 'use', required: false, type: String, description: "사용 여부 ('Y' 또는 'N')" })
+  getCouponsByEmployer(@Req() req: Request, @Query('use') use?: string) {
+    if (use && !['Y', 'N'].includes(use)) {
+      throw new BadRequestException('query parameter validation failed');
+    }
+
+    const isUsedFilter = use === 'Y' ? true : use === 'N' ? false : false;
+
+    return this.couponService.getCouponsByEmployer(req.user['sub'], isUsedFilter);
   }
 
-  @Get()
-  findAll() {
-    return this.couponService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.couponService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCouponDto: UpdateCouponDto) {
-    return this.couponService.update(+id, updateCouponDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.couponService.remove(+id);
+  @ApiOperation({ summary: '관리자 수동 쿠폰 발급' })
+  @Post('assign')
+  async assignCouponToEmployer(@Body() dto: CreateEmployerCouponDto) {
+    return this.couponService.assignCouponToEmployer(dto);
   }
 }
