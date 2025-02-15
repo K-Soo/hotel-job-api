e9197297fd4546513dfdd24c9d3f341348c1826f
K-Soo <kanabun102@naver.com>
2025-02-16 03:52:20 +0900

    feat: 헬멧 미들웨어 활성화

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
   const port = configService.get('PORT');
   const originUrls = configService.get('ORIGIN');
   // test
-  // app.use(helmet());
+  app.use(helmet());
 
   app.use(cookieParser());
 
