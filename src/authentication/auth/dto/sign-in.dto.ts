565d6158a016dd3833918a0caf2027ab0203fe15
K-Soo <kanabun102@naver.com>
2025-02-20 10:30:15 +0900

    feat: SignInDto의 예제 값을 빈 문자열로 수정하여 보안 강화

--- src/authentication/auth/dto/sign-in.dto.ts
+++ src/authentication/auth/dto/sign-in.dto.ts
@@ -3,9 +3,9 @@ import { CreateEmployerDto } from '../../../modules/employers/dto/create-employe
 import { PickType } from '@nestjs/mapped-types';
 
 export class SignInDto extends PickType(CreateEmployerDto, ['userId', 'password'] as const) {
-  @ApiProperty({ description: '아이디', example: 'kanabun102' })
+  @ApiProperty({ description: '아이디', example: '' })
   userId: string;
 
-  @ApiProperty({ description: '비밀번호', example: '@@EErr1234' })
+  @ApiProperty({ description: '비밀번호', example: '' })
   password: string;
 }
