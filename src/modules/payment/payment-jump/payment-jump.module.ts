3d1502b22ce4fbb57c48068b98b12c18f20b70f2
K-Soo <kanabun102@naver.com>
2025-02-25 18:08:34 +0900

    feat: 알림 및 결제 관련 모듈과 DTO, 서비스, 컨트롤러 삭제

--- src/modules/payment/payment-jump/payment-jump.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PaymentJumpService } from './payment-jump.service';
-import { PaymentJumpController } from './payment-jump.controller';
-
-@Module({
-  controllers: [PaymentJumpController],
-  providers: [PaymentJumpService],
-})
-export class PaymentJumpModule {}
--- src/modules/payment/payment-resume-view/payment-resume-view.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { PaymentResumeViewService } from './payment-resume-view.service';
-
-@Controller('payment-resume-view')
-export class PaymentResumeViewController {
-  constructor(private readonly paymentResumeViewService: PaymentResumeViewService) {}
-}
