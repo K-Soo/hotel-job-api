8892bcdb5e3d6475d5e304879f32f53ebb2aa121
K-Soo <kanabun102@naver.com>
2025-02-25 18:09:47 +0900

    fix: 불필요한 BadRequestException 임포트 제거

--- src/providers/toss/toss.service.ts
+++ src/providers/toss/toss.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, BadRequestException, Logger } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { HttpService } from '@nestjs/axios';
 import { lastValueFrom } from 'rxjs';
