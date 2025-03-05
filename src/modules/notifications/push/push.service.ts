5bc26be225e2622290d326890b020093e78285f8
K-Soo <kanabun102@naver.com>
2025-03-06 03:12:14 +0900

    feat: FCM 토큰 저장 메서드에 예외 처리 추가 및 로그 제거

--- src/modules/notifications/push/push.service.ts
+++ src/modules/notifications/push/push.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
 import { SaveFcmTokenDto } from './dto/save-fcm-token.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { In, LessThan, Repository } from 'typeorm';
@@ -22,7 +22,6 @@ export class PushService {
    * 푸시 알림 전송
    */
   async sendPushNotification(sendPushToUserDto: SendPushToUserDto) {
-    console.log('sendPushToUserDto: ', sendPushToUserDto);
     const tokens = await this.getUserPushTokens(sendPushToUserDto.userIds);
 
     if (tokens.length === 0) {
@@ -66,57 +65,59 @@ export class PushService {
   }
 
   async saveFcmToken(saveFcmTokenDto: SaveFcmTokenDto, userAgent: UserAgent, user: RequestUser) {
-    const existingToken = await this.pushRepo.findOne({ where: { token: saveFcmTokenDto.token } });
+    try {
+      const existingToken = await this.pushRepo.findOne({ where: { token: saveFcmTokenDto.token } });
+      console.log('existingToken: ', existingToken);
 
-    console.log('existingToken: ', existingToken.userId);
-    console.log('sub: ', user.sub);
+      // 사업자 5ef779c1-7eed-4fff-b155-1650bc2fe0a1
+      //유저 91aa8042-78e9-4cbb-a30d-f976f85c1078
+      //daxivcQIMPSbJvdGduH5IR:APA91bFfgI2b8_YRqlTJ78EEcII3x
 
-    // 사업자 5ef779c1-7eed-4fff-b155-1650bc2fe0a1
-    //유저 91aa8042-78e9-4cbb-a30d-f976f85c1078
-    //daxivcQIMPSbJvdGduH5IR:APA91bFfgI2b8_YRqlTJ78EEcII3x
+      if (existingToken) {
+        // 기존 토큰이 다른 사용자에게 등록된 경우 -> 사용자 변경
+        if (existingToken.userId !== user.sub) {
+          console.log('사용자 변경');
+          await this.pushRepo.update(existingToken.id, {
+            userId: user.sub,
+            device: userAgent.device,
+            os: userAgent.os,
+            updatedAt: new Date(),
+          });
 
-    if (existingToken) {
-      // 기존 토큰이 다른 사용자에게 등록된 경우 -> 사용자 변경
-      if (existingToken.userId !== user.sub) {
-        console.log('사용자 변경');
-        await this.pushRepo.update(existingToken.id, {
-          userId: user.sub,
-          device: userAgent.device,
-          os: userAgent.os,
-          updatedAt: new Date(),
-        });
+          return {
+            success: ResponseStatus.SUCCESS,
+            token: existingToken.token,
+            message: 'FCM token has been reassigned to a new user.',
+          };
+        }
 
-        return {
-          success: ResponseStatus.SUCCESS,
-          token: existingToken.token,
-          message: 'FCM token has been reassigned to a new user.',
-        };
+        // 기존 사용자와 동일하면 `updatedAt`만 갱신
+        if (existingToken.userId === user.sub) {
+          console.log('날짜만 변경');
+          await this.pushRepo.update(existingToken.id, { updatedAt: new Date() });
+
+          return {
+            success: ResponseStatus.SUCCESS,
+            message: 'FCM token has been updated successfully.',
+            token: existingToken.token,
+          };
+        }
       }
 
-      // 기존 사용자와 동일하면 `updatedAt`만 갱신
-      if (existingToken.userId === user.sub) {
-        console.log('날짜만 변경');
-        await this.pushRepo.update(existingToken.id, { updatedAt: new Date() });
+      // 새로운 사용자 ID로 푸시 토큰 저장
+      const newFcmToken = this.pushRepo.create({
+        userId: user.sub,
+        token: saveFcmTokenDto.token,
+        device: userAgent.device,
+        os: userAgent.os,
+      });
 
-        return {
-          success: ResponseStatus.SUCCESS,
-          message: 'FCM token has been updated successfully.',
-          token: existingToken.token,
-        };
-      }
+      await this.pushRepo.save(newFcmToken);
+
+      return { success: ResponseStatus.SUCCESS, token: saveFcmTokenDto.token };
+    } catch (error) {
+      throw new BadRequestException(error.message);
     }
-
-    // 새로운 사용자 ID로 푸시 토큰 저장
-    const newFcmToken = this.pushRepo.create({
-      userId: user.sub,
-      token: saveFcmTokenDto.token,
-      device: userAgent.device,
-      os: userAgent.os,
-    });
-
-    await this.pushRepo.save(newFcmToken);
-
-    return { success: ResponseStatus.SUCCESS, token: saveFcmTokenDto.token };
   }
 
   /**
