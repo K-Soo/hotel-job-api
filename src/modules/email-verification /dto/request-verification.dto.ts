5f806a4b9a9fa6b7d11b371dbb9ceb6e945717e0
K-Soo <kanabun102@naver.com>
2025-04-03 02:09:06 +0900

    feat: 이메일 인증 요청 DTO에 사용자 이름 및 리다이렉트 URL 필드 추가

--- src/modules/email-verification /dto/request-verification.dto.ts
+++ src/modules/email-verification /dto/request-verification.dto.ts
@@ -1,9 +1,20 @@
-import { IsEmail, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsString, Length, IsOptional } from 'class-validator';
+import { validationMessage } from '../../../common/utils/regex';
 
 export class RequestVerificationDto {
+  @ApiProperty({ description: '사업자 유저 이름', example: '고원호' })
   @IsString()
-  userId: string;
+  @IsOptional()
+  userName?: string | null;
 
+  @ApiProperty({ description: '담당자 이메일', example: 'kanabun102@naver.com' })
+  @Length(5, 40, validationMessage('email'))
+  @IsString()
   @IsEmail()
   email: string;
+
+  @ApiProperty({ description: '리다이렉트 URL', example: '/recover/account/success' })
+  @IsString()
+  redirect: string;
 }
