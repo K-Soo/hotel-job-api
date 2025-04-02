258857830718782233c075d9635a0b54c56ea0bc
K-Soo <kanabun102@naver.com>
2025-04-03 02:09:20 +0900

    feat: AuthModule을 알림 모듈에 추가

--- src/modules/notifications/notifications.module.ts
+++ src/modules/notifications/notifications.module.ts
@@ -6,6 +6,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Notification } from './entities/notification.entity';
 import { NotificationGateway } from '../../gateways/notification.gateway';
 import { AuthModule } from '../../authentication/auth/auth.module';
+
 @Global()
 @Module({
   imports: [TypeOrmModule.forFeature([Notification]), PushModule, AuthModule],
