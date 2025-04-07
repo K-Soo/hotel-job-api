97b6b40b9852455a44d5aff0da79474c0db34277
K-Soo <kanabun102@naver.com>
2025-04-07 20:31:42 +0900

    feat: 채용 상품 시더 코드 주석 처리

--- src/database/seeds/recruitment-main-product.seeder.ts
+++ src/database/seeds/recruitment-main-product.seeder.ts
@@ -1,83 +1,83 @@
-/* eslint-disable prefer-const */
-import { Seeder, SeederFactoryManager } from 'typeorm-extension';
-import { DataSource } from 'typeorm';
-import { RecruitmentProductType } from '../../common/constants/product';
-import { RecruitmentProduct } from '../../modules/products/entities/recruitment.entity';
-import { RecruitmentProductOption } from '../../modules/products/entities/recruitment-option.entity';
-import { RecruitmentProductOptionDuration } from '../../modules/products/entities/recruitment-option-duration.entity';
-import { RecruitmentProductDuration } from '../../modules/products/entities/recruitment-duration.entity';
-import { MAIN_PRODUCT_SEEDER } from '../../common/constants/seeder';
-export default class RecruitmentMainProductSeeder implements Seeder {
-  async run(dataSource: DataSource, _: SeederFactoryManager): Promise<void> {
-    const productRepository = dataSource.getRepository(RecruitmentProduct);
-    const optionRepository = dataSource.getRepository(RecruitmentProductOption);
-    const durationRepository = dataSource.getRepository(RecruitmentProductDuration);
-    const optionDurationRepository = dataSource.getRepository(RecruitmentProductOptionDuration);
+// /* eslint-disable prefer-const */
+// import { Seeder, SeederFactoryManager } from 'typeorm-extension';
+// import { DataSource } from 'typeorm';
+// import { RecruitmentProductType } from '../../common/constants/product';
+// import { RecruitmentProduct } from '../../modules/products/entities/recruitment.entity';
+// import { RecruitmentProductOption } from '../../modules/products/entities/recruitment-option.entity';
+// import { RecruitmentProductOptionDuration } from '../../modules/products/entities/recruitment-option-duration.entity';
+// import { RecruitmentProductDuration } from '../../modules/products/entities/recruitment-duration.entity';
+// import { MAIN_PRODUCT_SEEDER } from '../../common/constants/seeder';
+// export default class RecruitmentMainProductSeeder implements Seeder {
+//   async run(dataSource: DataSource, _: SeederFactoryManager): Promise<void> {
+//     const productRepository = dataSource.getRepository(RecruitmentProduct);
+//     const optionRepository = dataSource.getRepository(RecruitmentProductOption);
+//     const durationRepository = dataSource.getRepository(RecruitmentProductDuration);
+//     const optionDurationRepository = dataSource.getRepository(RecruitmentProductOptionDuration);
 
-    // 기존 데이터 삭제
-    await productRepository.delete({ type: RecruitmentProductType.MAIN });
+//     // 기존 데이터 삭제
+//     await productRepository.delete({ type: RecruitmentProductType.MAIN });
 
-    console.log('✅ 기존 데이터 삭제 완료! 새로운 데이터 추가 시작...');
+//     console.log('✅ 기존 데이터 삭제 완료! 새로운 데이터 추가 시작...');
 
-    for (const PRODUCT_ITEM of MAIN_PRODUCT_SEEDER) {
-      const existingProduct = await productRepository.findOne({
-        where: { type: PRODUCT_ITEM.type, name: PRODUCT_ITEM.name },
-      });
+//     for (const PRODUCT_ITEM of MAIN_PRODUCT_SEEDER) {
+//       const existingProduct = await productRepository.findOne({
+//         where: { type: PRODUCT_ITEM.type, name: PRODUCT_ITEM.name },
+//       });
 
-      if (existingProduct) {
-        // 옵션 기간 삭제
-        for (const option of existingProduct.options) {
-          await optionDurationRepository.delete({ option: { id: option.id } });
-        }
-        // 기존 옵션 삭제
-        await optionRepository.delete({ recruitmentProduct: existingProduct });
+//       if (existingProduct) {
+//         // 옵션 기간 삭제
+//         for (const option of existingProduct.options) {
+//           await optionDurationRepository.delete({ option: { id: option.id } });
+//         }
+//         // 기존 옵션 삭제
+//         await optionRepository.delete({ recruitmentProduct: existingProduct });
 
-        // 기존 기간 삭제
-        await durationRepository.delete({ recruitmentProduct: existingProduct });
-      }
+//         // 기존 기간 삭제
+//         await durationRepository.delete({ recruitmentProduct: existingProduct });
+//       }
 
-      // Step 1: 새 상품 생성
-      const savedProduct = await productRepository.save(
-        productRepository.create({
-          type: PRODUCT_ITEM.type,
-          name: PRODUCT_ITEM.name,
-        }),
-      );
+//       // Step 1: 새 상품 생성
+//       const savedProduct = await productRepository.save(
+//         productRepository.create({
+//           type: PRODUCT_ITEM.type,
+//           name: PRODUCT_ITEM.name,
+//         }),
+//       );
 
-      // Step 2: 상품 기간 추가
-      for (const durationData of PRODUCT_ITEM.durations) {
-        await durationRepository.save(
-          durationRepository.create({
-            recruitmentProduct: savedProduct,
-            ...durationData,
-          }),
-        );
-      }
+//       // Step 2: 상품 기간 추가
+//       for (const durationData of PRODUCT_ITEM.durations) {
+//         await durationRepository.save(
+//           durationRepository.create({
+//             recruitmentProduct: savedProduct,
+//             ...durationData,
+//           }),
+//         );
+//       }
 
-      // Step 3: 옵션 추가
-      for (const optionData of PRODUCT_ITEM.options) {
-        const createdOptions = optionRepository.create({
-          recruitmentProduct: savedProduct,
-          name: optionData.name,
-          maxListUpPerDay: optionData.maxListUpPerDay,
-          listUpIntervalHours: optionData.listUpIntervalHours,
-          tags: optionData.tags,
-        });
+//       // Step 3: 옵션 추가
+//       for (const optionData of PRODUCT_ITEM.options) {
+//         const createdOptions = optionRepository.create({
+//           recruitmentProduct: savedProduct,
+//           name: optionData.name,
+//           maxListUpPerDay: optionData.maxListUpPerDay,
+//           listUpIntervalHours: optionData.listUpIntervalHours,
+//           tags: optionData.tags,
+//         });
 
-        const savedOptions = await optionRepository.save(createdOptions);
+//         const savedOptions = await optionRepository.save(createdOptions);
 
-        // Step 4: 옵션 기간 추가
-        for (const optionDurationData of optionData.optionDurations) {
-          const createdOptionsDuration = optionDurationRepository.create({
-            option: savedOptions,
-            ...optionDurationData,
-          });
+//         // Step 4: 옵션 기간 추가
+//         for (const optionDurationData of optionData.optionDurations) {
+//           const createdOptionsDuration = optionDurationRepository.create({
+//             option: savedOptions,
+//             ...optionDurationData,
+//           });
 
-          await optionDurationRepository.save(createdOptionsDuration);
-        }
-      }
-    }
+//           await optionDurationRepository.save(createdOptionsDuration);
+//         }
+//       }
+//     }
 
-    console.log('메인 페이지 상품, 옵션 데이터 추가 완료');
-  }
-}
+//     console.log('메인 페이지 상품, 옵션 데이터 추가 완료');
+//   }
+// }
